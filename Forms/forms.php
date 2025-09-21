55573d888384f66f7835b161781d003458284a31
kadaji777 <162417406+kadaji777@users.noreply.github.com>
2025-09-21 14:17:37 +0300

    Update forms.php

--- Forms/forms.php
+++ Forms/forms.php
@@ -3,7 +3,7 @@ class forms {
     public function signup($conf, $ObjFncs) {
       $err = $ObjFncs->getMsg('errors'); print $ObjFncs->getMsg('msg');
  ?>
-            <h2>Sign Up Here</h2>
+            <h2>Sign Up Here </h2>
 <form action='' method='post' autocomplete="off">
   <div class="mb-3">
     <label for="fullname" class="form-label">Fullname</label>
